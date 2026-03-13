2b463dccc2dd76ea40dbe109c63fcdc12c36ec58
bmontado <165435553+bmontado@users.noreply.github.com>
2026-03-13 13:28:49 -0300

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import MusicDecayIntelligence from './MusicDecayIntelligence'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <MusicDecayIntelligence />
+  </React.StrictMode>
+)
